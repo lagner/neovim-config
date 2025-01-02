dfd5789bf68eaf79e098e7702be6d46412bae22a
Sergey Lagner <s.lagner@2gis.ru>
2025-01-02 11:17:05 +0700

    [theme] auto switch according to system theme

--- lua/lagner/plugins/theme.lua
+++ lua/lagner/plugins/theme.lua
@@ -1,7 +1,19 @@
 return {
-	"ellisonleao/gruvbox.nvim", 
-	priority = 1000 , 
-	config = function() vim.o.background = "dark" -- or "light" for light mode
-		vim.cmd([[colorscheme gruvbox]])
-	end,
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+        local res = vim.system({
+            'defaults', 'read', '-g', 'AppleInterfaceStyle',
+        }, { text = true }):wait()
+        local background = "light"
+        if res.code == 0 then
+            local system_theme_name = string.gsub(res.stdout, "%s+", "")
+            system_theme_name = string.lower(system_theme_name)
+            if system_theme_name == "dark" then
+                background = "dark"
+            end
+        end
+        vim.o.background = background
+        vim.cmd([[colorscheme gruvbox]])
+    end,
 }
